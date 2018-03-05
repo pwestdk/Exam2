0c330193efbac622a1270058b9738b9e16bbec58
Philip West <pwestdk@gmail.com>
2018-03-05 09:21:14 +0100

    push it to the limit

--- test/Impl/Impl/HandlerImplTest.java
+++ test/Impl/Impl/HandlerImplTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import ExamProject.Car;
+import Impl.CarImpl;
+import Impl.HandlerImpl;
 
 class HandlerImplTest {
 	
